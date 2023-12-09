1988c0df4d4ec0da8740c7f75d13b79714e624b3
MKen212 <46496040+MKen212@users.noreply.github.com>
2023-12-09 18:42:46 +0100

    Project 09 - Colour Generator (Part 1)

---
+++ 09-colour-generator/src/App.jsx
@@ -0,0 +1,17 @@
+
+function App() {
+  return (
+    <div>
+      <header className="title" style={{marginBottom: "3rem"}}>
+        <h2>React Project 09 - Colour Generator</h2>
+        <div className="title-underline"></div>
+      </header>
+
+      <main>
+
+      </main>
+    </div>
+  );
+};
+
+export default App;
